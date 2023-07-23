ca64b88358548f8c65264cf41669ca44395f8a7f
Darknab <darknab@live.fr>
2023-07-23 18:14:08 +0100

    Replace the results list with a form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,11 +7,11 @@ class FlightsController < ApplicationController
       redirect_to flights_path(departure_airport: params[:departure_airport], 
         arrival_airport: params[:arrival_airport], 
         from_date: params[:from_date], 
-        to_date: params[:to_date])
+        to_date: params[:to_date], tickets: params[:tickets])
   end
 
   def index
-    if params[:departure_airport].present? && params[:arrival_airport].present? && params[:from_date].present? && params[:to_date].present?
+    if !params.empty?
       @flights = Flight.where('departure_airport_id = ? AND arrival_airport_id = ? AND start_datetime >= ? AND start_datetime <= ?',
                               params[:departure_airport], params[:arrival_airport], params[:from_date], params[:to_date])
     
