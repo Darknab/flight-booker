4d796223309cfe83dd5c320553c0d6e3c94104fb
Darknab <darknab@live.fr>
2023-08-16 17:47:35 +0100

    Create a confirmation_mail method in the mailer and the correspondent views Add aline in development.rb to set a default host

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,10 @@
 class PassengerMailer < ApplicationMailer
+  def confirmation_mail
+    @booking = params[:booking]
+    @passengers = @booking.passengers
+    
+    @passenger.each do |passenger|
+      mail(to:passenger.email, subject: 'Booking complete!')
+    end
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.default_url_options = { host: 'http://127.0.0.1:3000/' }
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
